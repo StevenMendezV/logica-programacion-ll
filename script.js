0871df7a4c40afed2665adc6a7a384a0cb72835d
StevenMendezV <89426613+StevenMendezV@users.noreply.github.com>
2023-10-10 10:37:18 -0500

    Realizamos algunas limpiezas en el código

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ function convertirTemp(){
     const tempCelsius = document.getElementById("celsius");
     const tempFahrenheitDom =document.getElementById("fahrenheit");
     const tempKelvinDom = document.getElementById("kelvin");
+    
     const tempCelsiusValueString = tempCelsius.value;
-
-    
-
-    
     const regexTem = /[\d]{1,}/;
 
     let validacionTemp = regexTem.test(tempCelsiusValueString);
 
-    console.log(validacionTemp)
-
     if(validacionTemp){
-        console.log("hola")
         const tempCelsiusInt = parseInt(tempCelsiusValueString);
 
         const tempCelsiudToFahrenheit = ((9*tempCelsiusInt)/5) + 32;
@@ -23,8 +17,5 @@ function convertirTemp(){
 
         tempFahrenheitDom.textContent = tempCelsiudToFahrenheit;
         tempKelvinDom.textContent = tempCelsiusToKelvin;
-
-        
-
     }
 }
